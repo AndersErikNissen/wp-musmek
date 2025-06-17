c560a93b3f7baf57c4abc27f59f6b37288b9fc74
Anders Erik Nissen <anderseriknissen@gmail.com>
2025-06-17 22:18:24 +0200

    Update acf.php

--- themes/blank-theme/assets/php/acf.php
+++ themes/blank-theme/assets/php/acf.php
@@ -20,164 +20,29 @@ function custom_acf_json_filename( $filename, $post, $load_path ) {
 }
 add_filter( 'acf/json/save_file_name', 'custom_acf_json_filename', 10, 3 );
 
+// function custom_acf_json_save_paths( $paths, $post ) {
+//   $has_found_path = false;
 
-// Reference: https://www.advancedcustomfields.com/resources/register-fields-via-php/
-if( function_exists('acf_add_local_field_group') ):
+//   if ( str_starts_with( $post['title'], 'Section' ) || str_starts_with( $post['title'], 'Sektion' ) ) {
+//     $has_found_path = true;
+//     $paths[] = array ( get_stylesheet_directory() . '/acf/sections' );
+//   }
+  
+//   if ( !$has_found_path ) {
+//     $paths[] = array ( get_stylesheet_directory() . '/acf' );
+//   }
 
-function acf_group( string $name, ...$fields ) {
+//   return $paths;
+// }
+// add_filter( 'acf/json/save_paths', 'custom_acf_json_save_paths', 10, 2 );
 
-	acf_add_local_field_group( array (
-		'key' 		 => 'group_' . $name,
-		'title' 	 => 'Sektion: ' . $name,
-		'fields' 	 => array ( $fields ),
-		'location' => array (
-			array (
-				array (
-					'param'		 => 'post_type',
-					'operator' => '==',
-					'value' 	 => 'post',
-				),
-			),
-		),
-		'menu_order' => 0,
-	) );
-}
+// function my_acf_json_load_point( $paths ) {
+//     // Remove the original path (optional).
+//     unset($paths[0]);
 
+//     // Append the new path and return it.
+//     $paths[] = get_stylesheet_directory() . '/acf';
 
-$field_number = 1;
-
-// FIELD: TITLE
-$groups_with_title = array ( 1, 3 );
-
-foreach( $groups_with_title as $number ) :
-	acf_add_local_field( array(
-		'key' 	 => 'field_' . $field_number,
-		'label'  => 'Titel',
-		'name' 	 => 'title',
-		'type' 	 => 'text',
-		'parent' => 'group_' . $number,
-	) );
-endforeach;
-
-
-// FIELD: TEXT AREA
-$field_number++;
-
-$groups_with_text_area = array ( 1, 3 );
-
-foreach( $groups_with_text_area as $number ) :
-	acf_add_local_field( array(
-		'key' 	 => 'field_' . $field_number,
-		'label'  => 'Tekst felt',
-		'name' 	 => 'text_area',
-		'type' 	 => 'textarea',
-		'parent' => 'group_' . $number,
-	) );
-endforeach;
-
-// FIELD: LINK
-$field_number++;
-$groups_with_link = array ( 1, 3 );
-
-foreach( $groups_with_link as $number ) :
-	acf_add_local_field( array(
-		'key' 	 => 'field_3',
-		'label'  => 'Link',
-		'name' 	 => 'link',
-		'type' 	 => 'link',
-		'instructions' => '(Vises som en knap)',
-		'parent' => 'group_' . $number,
-	) );
-endforeach;
-
-// FIELD: IMAGES
-$images_conditional_logic = array ();
-
-for( $i = 1; $i <= 10; $i++ ) :
-	$field_number++;
-
-	$image = array (  
-		'key' 	 				=> 'field_' . $field_number,
-		'label'  				=> 'Billede (' . $i . ')',
-		'name' 	 				=> 'image',
-		'type' 	 				=> 'image',
-		'return_format' => 'id',
-		'parent' 				=> 'group_2',
-	);
-
-	if ( $i > 1 ) :
-		$images_conditional_logic[] = array (
-			'field' 	 => 'field_' . $field_number - 1,
-			'operator' => '!=empty',
-		);
-
-		$image['conditional_logic'] = array( $images_conditional_logic );
-	endif;
-
-	if ($i !== 10) :
-		$image['instructions'] = 'Vælg billede for at vise nyt billede felt (' . 10 - $i . ' felter tilbage).';
-	endif;
-
-	acf_add_local_field( $image );
-endfor;
-
-// SECTION: TEXT
-acf_add_local_field_group( array (
-	'key' 		 => 'group_1',
-	'title' 	 => 'Sektion: Tekst',
-	'fields' 	 => array (),
-	'location' => array (
-		array (
-			array (
-				'param'		 => 'post_type',
-				'operator' => '==',
-				'value' 	 => 'post',
-			),
-		),
-	),
-	'menu_order' => 0,
-) );
-
-// SECTION: GALLERY
-acf_add_local_field_group( array (
-	'key' 		 => 'group_2',
-	'title' 	 => 'Sektion: Galleri',
-	'fields' 	 => array (),
-	'location' => array (
-		array (
-			array (
-				'param'		 => 'post_type',
-				'operator' => '==',
-				'value' 	 => 'post',
-			),
-		),
-	),
-) );
-
-// SECTION: LIST
-acf_add_local_field_group( array (
-	'key' 		 => 'group_3',
-	'title' 	 => 'Sektion: Liste',
-	'fields' 	 => array (),
-	'location' => array (
-		array (
-			array (
-				'param'		 => 'post_type',
-				'operator' => '==',
-				'value' 	 => 'post',
-			),
-		),
-	),
-) );
-
-
-// LAV TIL INDSTILLINGER SIDEN!!!
-// LAV TIL INDSTILLINGER SIDEN!!!
-// LAV TIL INDSTILLINGER SIDEN!!!
-// LAV TIL INDSTILLINGER SIDEN!!!
-// LAV TIL INDSTILLINGER SIDEN!!!
-// LAV TIL INDSTILLINGER SIDEN!!!
-// LAV TIL INDSTILLINGER SIDEN!!!
-
-
-endif;
+//     return $paths;
+// }
+// add_filter( 'acf/settings/load_json', 'my_acf_json_load_point' );
