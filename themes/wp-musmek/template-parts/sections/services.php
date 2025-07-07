6b8432becf2986640466cef4eca4bdd6ff667ae9
Anders Erik Nissen <anderseriknissen@gmail.com>
2025-07-07 19:30:52 +0200

    Update services.php

--- themes/wp-musmek/template-parts/sections/services.php
+++ themes/wp-musmek/template-parts/sections/services.php
@@ -4,35 +4,35 @@ $data = get_section_data( 'section_services_', get_the_ID() );
 if ( $data && !empty( $data['services'] ) ) :
 
 $service_posts = $data['services'];
-$show_numbers = get_field( 'show_numbers' ); ?>
+$show_numbers = get_field( 'show_numbers' ); 
+
+if ( $service_posts ) : ?>
 
 <section class="section-services section">
   <div class="section-services__content cols-10">
-    <?php if ( $service_posts ) : ?>
-      <ul class="section-services__posts">
-        <?php foreach ($service_posts as $key => $post) :
-          setup_postdata( $post ); ?>
-          <li class="section-services__post">
-            <?php if ( $show_numbers ) : ?>
-              <p class="label">0<?= $key + 1; ?></p>
-            <?php endif; ?>
+    <ul class="section-services__posts">
+      <?php foreach ($service_posts as $key => $post) :
+        setup_postdata( $post ); ?>
+        <li class="section-services__post">
+          <?php if ( $show_numbers ) : ?>
+            <p class="label">0<?= $key + 1; ?></p>
+          <?php endif; ?>
 
-            <?php if ( get_field( 'title' ) ) : ?>
-              <h3 class="h2"><?php the_field( 'title' ); ?></h3>
-            <?php endif; ?>
+          <?php if ( get_field( 'title' ) ) : ?>
+            <h3 class="h2"><?php the_field( 'title' ); ?></h3>
+          <?php endif; ?>
 
-            <?php if ( get_field( 'short_description' ) ) : ?>
-              <p class="p"><?php the_field( 'short_description' ); ?></p>
-            <?php endif; ?>
+          <?php if ( get_field( 'short_description' ) ) : ?>
+            <p class="p"><?php the_field( 'short_description' ); ?></p>
+          <?php endif; ?>
 
-            <?php if ( get_field( 'read_more_link' ) ) : ?>
-              <a class="btn--link" href="<?php the_field( 'read_more_link' ); ?>">Læs mere</a>
-            <?php endif; ?>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    <?php wp_reset_postdata(); endif; ?>
+          <?php if ( get_field( 'read_more_link' ) ) : ?>
+            <a class="btn--link" href="<?php the_field( 'read_more_link' ); ?>">Læs mere</a>
+          <?php endif; ?>
+        </li>
+      <?php endforeach; wp_reset_postdata(); ?>
+    </ul>
   </div>
 </section>
 
-<?php endif;
+<?php endif; endif;
